d7b521a78ec4b7dd262a7a1cd584a12b10105dd3
kricks <ric12052@byui.edu>
2019-09-27 19:23:33 -0500

    added stringbuilder to reverseString class

--- src/ReverseString.java
+++ src/ReverseString.java
@@ -1,17 +1,30 @@
 import java.util.Scanner;
 
 public class ReverseString {
+
+    public static String iteration(String phrase) {
+        String reverse = " ";
+        for (int i = phrase.length() - 1; i >= 0; i--){
+            reverse = reverse + phrase.charAt(i);
+        }
+        return reverse;
+    }
+
+    public static StringBuilder withStringBuilder(String phrase) {
+        StringBuilder str = new StringBuilder();
+        str.append(phrase);
+        str = str.reverse();;
+
+        return str;
+    }
+
     public static void main(String[] args) {
         System.out.print("Enter a string to reverse: ");
 
         Scanner scan = new Scanner(System.in);
         String phrase = scan.nextLine();
-        String reverse = " ";
 
-        for (int i = phrase.length() - 1; i >= 0; i--){
-            reverse = reverse + phrase.charAt(i);
-        }
-
-        System.out.println("The reverse string is: " + reverse);
+        System.out.println("The reverse string using iteration is: " + iteration(phrase));
+        System.out.println("The reverse string using StringBuilder is: " + withStringBuilder(phrase));
     }
 }
